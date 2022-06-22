a6d8da8c40ac91d990a2e1ea6bb9d1d22aa90287
D1_62774_Prasanna <prasannachendke33@gmail.com>
2022-06-22 16:34:19 +0530

    isEven function

--- page1.js
+++ page1.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+function isEven(number)
+{
+    if(number%2==0){
+       console.log(`${number} is even`) 
+    }
+    else{
+        console.log(`${number} is not even`)
+    }
+}
+
+isEven(10)
+isEven(11)
